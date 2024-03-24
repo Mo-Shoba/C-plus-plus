22e5c248b9fc01e9584dccf143101e2593640b70
Mlondi <68794263+Mo-Shoba@users.noreply.github.com>
2024-03-24 22:25:44 +0200

    First C++

---
+++ c++.cpp
@@ -0,0 +1,10 @@
+//Hello world! program, for new C++ Programmers
+
+#include "SSDL.h"
+
+int main (int argc, char** argv)
+{
+    sout <<"Hello, world! (Press any key to quit.)\n";
+    SSDL_Waitkey(); // Wait for user to hit any key
+    return 0;
+}
